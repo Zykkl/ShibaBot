35bdad93f0555ab7fd5d6f4e23634b1c443e07ee
Zykkl <alex.zykl@pm.me>
2021-09-03 07:47:59 +0200

    Remove stuffs so bot only does it's thing

--- bot.js
+++ bot.js
@@ -24,7 +24,6 @@ client.on('message', message => {
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
-	const taggedUser = message.mentions.users.first();
 	const channel = client.channels.cache.get(message.channel.id);
 	if (command === 'shiba') {
 		fetch(ENDPOINTS.shiba, {
@@ -41,32 +40,6 @@ client.on('message', message => {
 					embeds: [embed],
 				});
 			});
-	}
-
-	else if (command === 'avatar') {
-		if (!message.mentions.users.size) {
-			const ownAvatarEmbed = new MessageEmbed()
-				.setImage(message.author.displayAvatarURL())
-				.setTitle(message.author.username + '\'s Avatar');
-			// return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
-			return message.channel.send({ embeds: [ownAvatarEmbed] });
-		}
-		return message.channel.send(`${taggedUser.username}'s avatar link: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
-	}
-
-	else if (command === 'prune') {
-		const amount = parseInt(args[0]) + 1;
-		if (isNaN(amount)) {
-			return message.reply('that doesn\'t seem to be a valid number.');
-		}
-		else if (amount <= 2) {
-			return message.reply('Number needs to be higher than 2');
-		}
-		else if (amount >= 100) {
-			return message.reply('Number needs to be lower than 100');
-		}
-		message.channel.bulkDelete(amount);
-		return message.reply('Pruned ' + (amount - 1) + ' messages!');
 	}
 });
 
