f10db1104ae4a9e30c93809bddd690ddeb318a69
Alexandre Carvalho <alex.zykl@pm.me>
2021-10-09 22:58:06 +0200

    Bot status

--- bot.js
+++ bot.js
@@ -2,31 +2,36 @@ import {} from 'dotenv/config'
 import Eris from 'eris'
 const bot = new Eris(process.env.TOKEN)
 const ENDPOINTS = {
-    shiba: 'http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true',
+	shiba: 'http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true',
 }
 import fetch from 'node-fetch';
 bot.on("ready", () => {
-    console.log("Ready!")
+	console.log("Ready!")
 });
+bot.editStatus(process.env.STATUS, {
+	type: 2,
+	name: process.env.STATUS_MESSAGE
+})
+
 
 bot.on("messageCreate", (msg => {
-    if (msg.content === "s.shiba") {
-        fetch(ENDPOINTS.shiba, {
-                method: 'GET',
-            })
-            .then(res => res.json())
-            .then(([data]) => {
-                bot.createMessage(msg.channel.id, {
-                    embed: {
-                        title: "Shiba!",
-                        url: data.url,
-                        color: 0xE67E22,
-                        image: {
-                            url: data
-                        }
-                    }
-                })
-            })
-    }
+	if (msg.content === "s.shiba") {
+		fetch(ENDPOINTS.shiba, {
+				method: 'GET',
+			})
+			.then(res => res.json())
+			.then(([data]) => {
+				bot.createMessage(msg.channel.id, {
+					embed: {
+						title: "Shiba!",
+						url: data.url,
+						color: 0xE67E22,
+						image: {
+							url: data
+						}
+					}
+				})
+			})
+	}
 }))
 bot.connect()
